bd1eac59809676ac19927f01b35c27f930a65ba5
rvwdude <rvwdude@gmail.com>
2019-04-17 10:10:31 +0300

    commitme

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,37 +1,14 @@
 import '../styles/index.scss';
 
-// const toggler = document.querySelectorAll('.nav__toggler');
-// const menu = document.querySelector('.nav__menu');
+const toggler = document.querySelector('.nav__toggler');
+const menu = document.querySelector('.nav__menu');
 
-// toggler.addEventListener('click', menuToggle);
+toggler.addEventListener('click', menuToggle);
 
-// function menuToggle(e) {
-//   e.preventDefault();
-//   menu.classList.toggle('open');
-// }
-
-
-
-// +++++++++++++++
-
-
-// function classToggle() {
-//   const navs = document.querySelectorAll('.Navbar__Items')
-  
-//   navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-// }
-
-// document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
-
-function menuToggle() {
-  const menus = document.querySelectorAll('.nav__menu');
-
-  menus.forEach(menu => {
-    menu.classList.toggle('open');
-  });
+function menuToggle(e) {
+  e.preventDefault();
+  menu.classList.toggle('open');
 }
 
-const togglers = document.querySelectorAll('.nav__toggler');
-togglers.forEach(toggler => {
-  toggler.addEventListener('click', menuToggle);
-});
+
+
