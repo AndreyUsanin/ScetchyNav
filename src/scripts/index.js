d5812be3828c464823f1da5571b4c20350592f55
rvwdude <rvwdude@gmail.com>
2019-04-29 11:26:49 +0300

    offside

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,27 @@
 import '../styles/index.scss';
 
+import offside from 'offside-js';
 
-const toggler = document.querySelector('.nav__toggler');
-const menu = document.querySelector('.nav__menu');
 
-toggler.addEventListener('click', menuToggle);
 
-function menuToggle(e) {
-  e.preventDefault();
-  menu.classList.toggle('open');
-}
+var myOffside = offside(".nav-offside", {
+  slidingElementsSelector: ".offside__container",
+  buttonsSelector: ".nav__toggler, .offside__close",
+  beforeOpen: function () {
+    document.body.classList.add('offside-js--interact');
+  },
 
+  afterOpen: function () {
+    document.body.classList.add('overflow-y');
+  },
+  beforeClose: function () {
+
+  },
+  afterClose: function () {
+    document.body.classList.remove('offside-js--interact');
+    document.body.classList.remove('overflow-y');
+  },
+});
+
+var overlay = document.querySelector(".page-overlay");
+overlay.addEventListener("click", myOffside.closeAll);
